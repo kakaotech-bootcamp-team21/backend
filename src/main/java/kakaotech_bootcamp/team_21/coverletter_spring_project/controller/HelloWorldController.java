e96e87d42df02fb84943baf732d31055e0eaa422
han-yohan <gksdygks359@naver.com>
2024-08-14 16:15:08 +0900

    change HelloWorldController!

--- src/main/java/kakaotech_bootcamp/team_21/coverletter_spring_project/controller/HelloWorldController.java
+++ src/main/java/kakaotech_bootcamp/team_21/coverletter_spring_project/controller/HelloWorldController.java
@@ -22,6 +22,6 @@ public class HelloWorldController {
     @ResponseBody//
     public String helloworld() {
         helloWorldService.helloTest();
-        return "Hello World!?!?";//Test API
+        return "Hello World Welcome!";//Test API
     }
 }
